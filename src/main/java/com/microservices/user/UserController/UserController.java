bcb016d72fa2d9ba873bf7e2a6654fe3f9dc80b7
Zailta <mannanbhat60@gmail.com>
2024-01-01 20:23:29 +0530

    User Microservice Controller

--- src/main/java/com/microservices/user/UserController/UserController.java
+++ src/main/java/com/microservices/user/UserController/UserController.java
@@ -1,9 +1,44 @@
 package com.microservices.user.UserController;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservices.user.UserBean.UserBean;
+import com.microservices.user.UserService.UserServiceInterface.UserService;
+
+@RestController
+@RequestMapping(value = "/microservice/user")
 public class UserController {
 
-	public UserController() {
-		// TODO Auto-generated constructor stub
+	@Autowired
+	UserService service;
+	//create user
+	
+	@PostMapping(value = "/create")
+	public ResponseEntity<UserBean> createUser( @RequestBody UserBean bean){
+		UserBean creatdUser = service.createUser(bean);
+		return ResponseEntity.ok(creatdUser);
 	}
-
+	//findSingle User
+	@GetMapping(value = "/{userId}")
+	public ResponseEntity<UserBean> findUser( @PathVariable String userId){
+		UserBean singleUser = service.getSingleUser(userId);
+		return ResponseEntity.ok(singleUser);
+	}
+	
+	//FindAllusers
+	@GetMapping(value = "/")
+	public ResponseEntity<List<UserBean>> findAll(){
+		List<UserBean> allIUsers = service.getAllIUsers();
+		return ResponseEntity.ok(allIUsers);
+	}
 }
+
--- src/main/java/com/microservices/user/UserRepository/UserRepository.java
+++ src/main/java/com/microservices/user/UserRepository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.microservices.user.UserRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.microservices.user.UserEntity.UserEntity;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, String>{
 
 	
