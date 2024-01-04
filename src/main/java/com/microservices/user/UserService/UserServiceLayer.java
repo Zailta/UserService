59b2c403e76e6165e3c0d4b7563924495e9a56f3
Zailta <mannanbhat60@gmail.com>
2024-01-04 13:39:26 +0530

    User Service changes to grab details from Multiple Microservice

--- src/main/java/com/microservices/user/UserService/UserServiceLayer.java
+++ src/main/java/com/microservices/user/UserService/UserServiceLayer.java
@@ -1,22 +1,28 @@
 package com.microservices.user.UserService;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.microservices.user.Exception.CustomExceptions.EXNResourceNotFoundException;
 import com.microservices.user.UserEntity.UserEntity;
 import com.microservices.user.UserRepository.UserRepository;
 import com.microservices.user.UserService.UserServiceInterface.UserService;
+import com.microservices.user.Userutility.UserHotel;
+import com.microservices.user.Userutility.UserRatings;
 
 
 @Service
 public class UserServiceLayer implements UserService{
 	@Autowired
 	UserRepository repository;
+	@Autowired
+	RestTemplate restTemplate;
 
 	@Override
 	public UserEntity createUser(UserEntity bean) {
@@ -27,6 +33,15 @@ public class UserServiceLayer implements UserService{
 	@Override
 	public UserEntity getSingleUser(String userid) {
 		UserEntity userEntity = repository.findById(userid).orElseThrow(()->new EXNResourceNotFoundException("User", "ID", userid));
+		UserRatings[] userratings = restTemplate.getForObject("http://RATING-SERVICE/ratingService/microservice/rating/fromuser/"+userid, UserRatings[].class);
+		List<UserRatings> userRatinglist = Arrays.stream(userratings).toList();
+		List<UserRatings> ratingsObject = userRatinglist.stream().map(rating->{
+			UserHotel userHotel = restTemplate.getForObject("http://HOTEL-SERVICE/hotelService/microservice/hotel/"+rating.getHotelId(), UserHotel.class);
+			rating.setHotel(userHotel);
+			return rating;
+		}).collect(Collectors.toList());
+		
+		userEntity.setRatings(ratingsObject);
 		return userEntity;
 	}
 
